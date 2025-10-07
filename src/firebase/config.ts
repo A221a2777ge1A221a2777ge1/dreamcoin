67232c43bb6794883ad0fc7fd665d01b81334d47
Ika NorthEast Government <ikanortheastgovernment@gmail.com>
2025-10-07 18:41:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4087474201-9833c",
+  "appId": "1:480210262299:web:d4a94f413d927d27cb9bbe",
+  "apiKey": "AIzaSyAJTCWb-AxiDoOeOUjfglXBgDgaIyzgnlE",
+  "authDomain": "studio-4087474201-9833c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "480210262299"
+};
